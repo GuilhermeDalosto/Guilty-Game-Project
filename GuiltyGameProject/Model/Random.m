525794802988a486a1a27787f1d95287695d420f
GMartinz <gmdalosto@gmail.com>
2019-11-25 17:56:35 -0300

    fixed MVC

--- GuiltyGameProject/Model/Random.m
+++ GuiltyGameProject/Model/Random.m
@@ -1,16 +1,16 @@
 //
-//  NSObject+Sorteio.m
+//  NSObject+Random.m
 //  GuiltyGameProject
 //
 //  Created by Guilherme Martins Dalosto de Oliveira on 25/11/19.
 //  Copyright © 2019 Guilherme Martins Dalosto de Oliveira. All rights reserved.
 //
 
-#import "NSObject+Sorteio.h"
+#import "Random.h"
 
 #import <AppKit/AppKit.h>
 
 
-@implementation NSObject (Sorteio)
+@implementation NSObject (Random)
 
 @end
