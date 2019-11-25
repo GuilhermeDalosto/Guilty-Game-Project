4ad108a98637d70c1ea9e69cc3092786d8084acf
GMartinz <gmdalosto@gmail.com>
2019-11-25 17:08:37 -0300

    added .gitignore

---
+++ GuiltyGameProject/Model/Sorteio.h
@@ -0,0 +1,20 @@
+//
+//  NSObject+Sorteio.h
+//  GuiltyGameProject
+//
+//  Created by Guilherme Martins Dalosto de Oliveira on 25/11/19.
+//  Copyright © 2019 Guilherme Martins Dalosto de Oliveira. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSObject (Sorteio)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GuiltyGameProject/Model/Sorteio.m
@@ -0,0 +1,16 @@
+//
+//  NSObject+Sorteio.m
+//  GuiltyGameProject
+//
+//  Created by Guilherme Martins Dalosto de Oliveira on 25/11/19.
+//  Copyright © 2019 Guilherme Martins Dalosto de Oliveira. All rights reserved.
+//
+
+#import "NSObject+Sorteio.h"
+
+#import <AppKit/AppKit.h>
+
+
+@implementation NSObject (Sorteio)
+
+@end
