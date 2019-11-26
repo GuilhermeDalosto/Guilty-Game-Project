f7384347312aeb40360a9e218169ed4a6a83d65b
GMartinz <gmdalosto@gmail.com>
2019-11-26 17:12:37 -0300

    created modules

--- GuiltyGameProject/Model/Person.h
+++ GuiltyGameProject/Model/Person.h
@@ -1,17 +1,19 @@
 //
-//  NSObject+Random.h
+//  NSObject+Person.h
 //  GuiltyGameProject
 //
-//  Created by Guilherme Martins Dalosto de Oliveira on 25/11/19.
+//  Created by Guilherme Martins Dalosto de Oliveira on 26/11/19.
 //  Copyright © 2019 Guilherme Martins Dalosto de Oliveira. All rights reserved.
 //
-
 
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface NSObject (Random)
+@interface Person : NSObject
+
+@property NSString *cor;
+//@property Team team;
 
 @end
 
